a5ee3c09be1c62a509ae7296d65e9b849242059b
Graham Krizek <graham@krizek.io>
2020-07-11 22:29:00 -0500

    [lnencrypt] Moves the crypto functions in the chanbackup package into its own package called lnencrypt
    The functions inside of the crypto.go file in chanbackup (like EncryptPayloadToWriter and DecryptPayloadFromReader) can be used by a lot of things outside of just the chanbackup package. We can't just reference them directly from the chanbackup package because it's likely that it would generate circular dependencies. Therefore we need to move these functions into their own package to be referenced by chanbackup and whatever new functionality that needs them

--- lnencrypt/crypto.go
+++ lnencrypt/crypto.go
@@ -1,4 +1,4 @@
-package chanbackup
+package lnencrypt
 
 import (
 	"bytes"
@@ -50,13 +50,13 @@ func genEncryptionKey(keyRing keychain.KeyRing) ([]byte, error) {
 	return encryptionKey[:], nil
 }
 
-// encryptPayloadToWriter attempts to write the set of bytes contained within
+// EncryptPayloadToWriter attempts to write the set of bytes contained within
 // the passed byes.Buffer into the passed io.Writer in an encrypted form. We
 // use a 24-byte chachapoly AEAD instance with a randomized nonce that's
 // pre-pended to the final payload and used as associated data in the AEAD. We
 // use the passed keyRing to generate the encryption key, see genEncryptionKey
 // for further details.
-func encryptPayloadToWriter(payload bytes.Buffer, w io.Writer,
+func EncryptPayloadToWriter(payload bytes.Buffer, w io.Writer,
 	keyRing keychain.KeyRing) error {
 
 	// First, we'll derive the key that we'll use to encrypt the payload
@@ -96,15 +96,14 @@ func encryptPayloadToWriter(payload bytes.Buffer, w io.Writer,
 	return nil
 }
 
-// decryptPayloadFromReader attempts to decrypt the encrypted bytes within the
+// DecryptPayloadFromReader attempts to decrypt the encrypted bytes within the
 // passed io.Reader instance using the key derived from the passed keyRing. For
 // further details regarding the key derivation protocol, see the
 // genEncryptionKey method.
-func decryptPayloadFromReader(payload io.Reader,
+func DecryptPayloadFromReader(payload io.Reader,
 	keyRing keychain.KeyRing) ([]byte, error) {
 
-	// First, we'll re-generate the encryption key that we use for all the
-	// SCBs.
+	// First, we'll re-generate the encryption key
 	encryptionKey, err := genEncryptionKey(keyRing)
 	if err != nil {
 		return nil, err
@@ -125,8 +124,7 @@ func decryptPayloadFromReader(payload io.Reader,
 	ciphertext := packedBackup[chacha20poly1305.NonceSizeX:]
 
 	// Now that we have the cipher text and the nonce separated, we can go
-	// ahead and decrypt the final blob so we can properly serialized the
-	// SCB.
+	// ahead and decrypt the final blob so we can properly serialize.
 	cipher, err := chacha20poly1305.NewX(encryptionKey)
 	if err != nil {
 		return nil, err
