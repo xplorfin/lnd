aca49e50f0ac67d76759f4d9c3072c69853ac06d
jalavosus <james@entropy.rocks>
2021-01-16 22:42:12 -0500

    Bump alpha version

--- build/version.go
+++ build/version.go
@@ -48,7 +48,7 @@ const (
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	AppPreRelease = "alpha.4"
+	AppPreRelease = "alpha.6"
 )
 
 // Version returns the application version as a properly formed string per the
